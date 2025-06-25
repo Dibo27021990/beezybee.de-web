72e59d9b4984432102cedb6a07d9888989bb948c
Dibo27021990 <Dirkbock90@web.de>
2025-06-25 14:38:08 +0200

    Create FlyingBee.jsx

---
+++ components/FlyingBee.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+export default function FlyingBee() {
+  return (
+    <motion.div
+      className="fixed bottom-8 right-8 z-50"
+      animate={{ y: [0, -10, 0] }}
+      transition={{ repeat: Infinity, duration: 2 }}
+    >
+      <Image
+        src="/images/flyingbee.png"
+        alt="Flying Bee"
+        width={64}
+        height={64}
+      />
+    </motion.div>
+  );
+}
