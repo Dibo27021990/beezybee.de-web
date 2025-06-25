cf40b2d5e82e2af8caa5f2d93d420bf594c25e50
Dibo27021990 <Dirkbock90@web.de>
2025-06-25 13:29:09 +0200

    Create page.jsx

---
+++ app/rewards/page.jsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+import RewardInfo from '@/components/RewardInfo';
+
+export default function RewardsPage() {
+  return (
+    <>
+      <Header />
+      <RewardInfo />
+    </>
+  );
+}
