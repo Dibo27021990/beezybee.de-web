822ad9af2d3cbe7bdf6c5452ea765c7d503bf645
Dibo27021990 <Dirkbock90@web.de>
2025-06-25 15:27:22 +0200

    Update SplashScreen.jsx

--- components/SplashScreen.jsx
+++ components/SplashScreen.jsx
@@ -1,16 +1,16 @@
 'use client';
 import Image from 'next/image';
-import { useTranslation } from 'react-i18next';
 
 export default function SplashScreen() {
-  const { t } = useTranslation();
-
   return (
-    <div className="flex items-center justify-center h-screen bg-yellow-400">
-      <div className="text-center">
-        <Image src="/images/splash.png" alt="Splash Logo" width={200} height={200} className="mx-auto" />
-        <p className="text-xl mt-4 font-bold text-gray-800">{t('splash.slogan')}</p>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-yellow-300">
+      <Image
+        src="/images/splash.png"
+        alt="Helperbee Splash"
+        width={300}
+        height={300}
+        priority
+      />
     </div>
   );
 }
