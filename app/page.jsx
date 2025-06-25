2ee1837ea136eed68835f0ba3ed9211b373237ef
Dibo27021990 <Dirkbock90@web.de>
2025-06-25 15:27:44 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import SplashScreen from '@/components/SplashScreen';
 import Hero from '@/components/Hero';
 import HowItWorks from '@/components/HowItWorks';
 import ForWhom from '@/components/ForWhom';
