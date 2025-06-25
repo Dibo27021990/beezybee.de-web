9ad7d6c1ac5c93325a937a71bfe62a835ebe84b7
Dibo27021990 <Dirkbock90@web.de>
2025-06-25 13:46:32 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,23 @@
 import './globals.css';
-import Navbar from '@/components/Navbar';
+import StickyHeader from '@/components/StickyHeader';
 import { dir } from 'i18next';
-import { languages } from '../i18n/settings';
-
-export const metadata = {
-  title: 'Helperbee',
-  description: 'Die App, die wirklich hilft.',
-};
+import { languages } from '@/i18n/settings';
 
 export function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
 }
 
+// Keine festen Texte mehr hier!
+export const metadata = {
+  title: 'Helperbee',
+};
+
 export default function RootLayout({ children, params: { lng } }) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <body>
-        <Navbar />
-        <main className="pt-16">{children}</main>
+        <StickyHeader lng={lng} />
+        {children}
       </body>
     </html>
   );
