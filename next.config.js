d72c9e878daeb0f28606b87a8050a33583c1ee20
Dibo27021990 <Dirkbock90@web.de>
2025-06-29 03:09:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-const { i18n } = require('./next-i18next.config');
+// next.config.js
+const { i18n } = require('./lib/i18n');
 
 module.exports = {
   reactStrictMode: true,
   i18n,
-  experimental: {
-    serverComponentsExternalPackages: ['undici'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      module: false,
-      stream: false,
-      assert: false
-    };
-    return config;
-  }
 };
