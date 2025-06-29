a718210516aa2d0a0c45cfb4c6dace4194f18e3d
Dibo27021990 <Dirkbock90@web.de>
2025-06-29 02:27:43 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,13 @@ import '@/styles/globals.css';
 import StickyHeader from '@/components/StickyHeader';
 import { dir } from 'i18next';
 import { languages } from '@/lib/i18n';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/lib/i18n';
 
 export function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
 }
 
-// Keine festen Texte mehr hier!
 export const metadata = {
   title: 'Helperbee',
 };
@@ -16,8 +17,10 @@ export default function RootLayout({ children, params: { lng } }) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <body>
-        <StickyHeader lng={lng} />
-        {children}
+        <I18nextProvider i18n={i18n}>
+          <StickyHeader lng={lng} />
+          {children}
+        </I18nextProvider>
       </body>
     </html>
   );
