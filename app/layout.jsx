f6074c8b705e06da4cd8daa6913a3171fabe99bd
Dibo27021990 <Dirkbock90@web.de>
2025-06-29 02:35:26 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,6 @@ import '@/styles/globals.css';
 import StickyHeader from '@/components/StickyHeader';
 import { dir } from 'i18next';
 import { languages } from '@/lib/i18n';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '@/lib/i18n';
 
 export function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -17,10 +15,8 @@ export default function RootLayout({ children, params: { lng } }) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <body>
-        <I18nextProvider i18n={i18n}>
-          <StickyHeader lng={lng} />
-          {children}
-        </I18nextProvider>
+        <StickyHeader lng={lng} />
+        {children}
       </body>
     </html>
   );
