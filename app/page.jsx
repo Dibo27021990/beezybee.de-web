b8a6c4d92222107ad02d899e1f9218fadeea2819
Dibo27021990 <Dirkbock90@web.de>
2025-06-29 02:28:31 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -91,12 +91,5 @@ export default function WaitlistPage() {
         <section id="screenshots" className="py-8 bg-white">
           <h2 className="text-xl font-bold text-center mb-8">{t('screenshots.title')}</h2>
           <div className="flex flex-wrap gap-4 justify-center">
-            <img src="/public/images/appscreen1.png" alt={t('screenshots.alt1')} className="w-[250px] rounded-xl" />
-            <img src="/public/images/appscreen2.png" alt={t('screenshots.alt2')} className="w-[250px] rounded-xl" />
-            <img src="/public/images/appscreen3.png" alt={t('screenshots.alt3')} className="w-[250px] rounded-xl" />
-          </div>
-        </section>
-      </main>
-    </>
-  );
-}
+            <img src="/images/appscreen1.png" alt={t('screenshots.alt1')} className="w-[250px] rounded-xl" />
+            <img src="/images/appscreen2.png" alt={t('screenshots.alt2')} className="w-[250px] rou
