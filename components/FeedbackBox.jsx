5b39ae82f6b0f4e16cf9c00d48b4fdfa47233d2c
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 10:13:00 +0200

    Delete components directory

--- components/FeedbackBox.jsx
+++
@@ -1,8 +0,0 @@
-export default function FeedbackBox() {
-  return (
-    <div className="bg-[#f4eeb1] p-4 mt-8 rounded shadow">
-      <h3 className="font-semibold mb-2">Dein Feedback hilft uns!</h3>
-      <p>Sag uns, was dir gefällt oder was wir besser machen können.</p>
-    </div>
-  );
-}
