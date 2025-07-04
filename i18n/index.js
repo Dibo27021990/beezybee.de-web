6a0a34af23752408a34b6517c5773b59759b1f20
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 10:27:30 +0200

    Update index.js

--- i18n/index.js
+++ i18n/index.js
@@ -1,8 +1,8 @@
 // i18n/index.js (i18next-Setup für DE/EN, Commons-Dateien importiert)
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import commonDe from '../public/locales/common.de.json';
-import commonEn from '../public/locales/common.en.json';
+import commonDe from '../public/locales/de/common.json';
+import commonEn from '../public/locales/en/common.json';
 
 i18n
   .use(initReactI18next)
