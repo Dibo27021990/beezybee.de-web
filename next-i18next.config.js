c48c7fa3689e64eb00b9fe3cdc462a0049c07b92
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 10:53:38 +0200

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,7 @@
-// next-i18next.config.js
-/** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'de',
     locales: ['de', 'en'],
+    localePath: './public/locales',
   },
 };
