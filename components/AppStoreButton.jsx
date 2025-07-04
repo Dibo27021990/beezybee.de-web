fcc619c968ec1de0597fd11374a8d62b09522ff2
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 08:42:38 +0200

    Update AppStoreButton.jsx

--- components/AppStoreButton.jsx
+++ components/AppStoreButton.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useTranslations } from 'next-intl';
+import { useTranslation } from 'react-i18next';
 
 export default function AppStoreButtons() {
   const { t } = useTranslation();
