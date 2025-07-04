addb17f161078fce145927b8ce1f258e2ae6d4fb
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 10:14:45 +0200

    Delete utils directory

--- utils/rewards.js
+++
@@ -1,8 +0,0 @@
-// utils/rewards.js (Rewards/Leaderboard-Logik, Dummy für Demo)
-export async function fetchLeaderboard() {
-  return [
-    { username: 'BeeFan', coins: 310, rank: 1, avatar: '/assets/avatar1.png' },
-    { username: 'Buzzing', coins: 280, rank: 2, avatar: '/assets/avatar2.png' },
-    { username: 'Dirk', coins: 250, rank: 3, avatar: '/assets/avatar3.png' },
-  ];
-}
