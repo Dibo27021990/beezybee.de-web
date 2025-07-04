7c48a6db22fc1a5ae68cfb9dfd5bf43f98e5d24a
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 11:28:51 +0200

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,18 +1,4 @@
-import Link from 'next/link';
-
+// app/not-found/page.jsx
 export default function NotFound() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center px-5 bg-white">
-      <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-      <p className="text-lg text-gray-800 mb-8 text-center">
-        Seite nicht gefunden.
-      </p>
-      <Link
-        href="/"
-        className="bg-gray-800 text-white px-6 py-3 rounded hover:bg-gray-700 transition"
-      >
-        Zur Startseite
-      </Link>
-    </div>
-  );
+  return <div>404 – Seite nicht gefunden</div>;
 }
