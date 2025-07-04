0a3c982182e6482b1d03c9ead0bcf448ec00f3cb
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 10:23:20 +0200

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Footer() {
-  return (
-    <footer className="bg-[#333] text-white py-4 text-center text-sm">
-      <p>&copy; {new Date().getFullYear()} Helperbee – All rights reserved.</p>
-    </footer>
-  );
-}
