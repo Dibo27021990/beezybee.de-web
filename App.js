646d6b3dd5395030a7ecdede6addd41846feeaea
Dibo27021990 <Dirkbock90@web.de>
2025-07-04 08:52:31 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import i18n from '../../i18n/index';
 import MainNavigator from './MainNavigator';
 import { colors } from './styles/theme';
 
