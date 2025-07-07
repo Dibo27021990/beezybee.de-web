b16049fb888d7d8ebda4625734398022cae77be8
Dibo27021990 <Dirkbock90@web.de>
2025-07-07 18:14:41 +0200

    Delete app/not-found directory

--- app/not-found/page.jsx
+++
@@ -1,9 +0,0 @@
-// app/not-found/page.jsx
-export default function NotFound() {
-  return (
-    <div style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>404 – Seite nicht gefunden</h1>
-      <p>Die Seite existiert nicht oder wurde verschoben.</p>
-    </div>
-  );
-}
