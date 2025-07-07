12dd049c7f92cd70d25c525b3e324ea15982b426
Dibo27021990 <Dirkbock90@web.de>
2025-07-07 16:22:40 +0200

    Update index.js

--- i18n/index.js
+++ i18n/index.js
@@ -2,11 +2,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 
+// Default auf Deutsch
+let defaultLng = 'de';
+if (typeof window !== 'undefined') {
+  const host = window.location.hostname;
+  if (host.endsWith('.com')) defaultLng = 'en';
+  if (host.endsWith('.de')) defaultLng = 'de';
+}
+
 i18n
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'de',
+    lng: defaultLng,           // <--- Das ist neu!
+    fallbackLng: defaultLng,   // <--- Optional, aber sauber!
     supportedLngs: ['de', 'en'],
     ns: ['common'],
     defaultNS: 'common',
