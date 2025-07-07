4a8eedbdfa28ee31aa07ad025d42324faeefd3a2
Dibo27021990 <Dirkbock90@web.de>
2025-07-07 17:10:41 +0200

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,10 +1,3 @@
-// app/not-found.jsx
-'use client';
-
 export default function NotFound() {
-  return (
-    <div>
-      <h1>404 – Seite nicht gefunden</h1>
-    </div>
-  );
+  return <div>404 – Seite nicht gefunden</div>;
 }
