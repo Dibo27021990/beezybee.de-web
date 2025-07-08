8af119778a2b34db67229dfeed0fb35da1fbadbb
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 10:54:10 +0200

    Update page.jsx

--- app/bureaucracy/page.jsx
+++ app/bureaucracy/page.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import StickyHeader from '@/components/StickyHeader';
 import dynamic from 'next/dynamic';
 import { useTranslation } from 'react-i18next';
