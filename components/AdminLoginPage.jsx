e2d2f6a4e51de9ab648696b6b84206e0377d6bb4
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 16:04:03 +0200

    Update AdminLoginPage.jsx

--- components/AdminLoginPage.jsx
+++ components/AdminLoginPage.jsx
@@ -1,4 +1,10 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
+import StickyHeader from '@/components/StickyHeader';
+import { useTranslation } from 'react-i18next';
+
+export default function RewardsPage() { /* ... */ }
 
 import { useState } from 'react';
 import { auth } from '@/lib/firebase';
