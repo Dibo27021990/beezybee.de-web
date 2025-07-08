e67e14182de0499111664d6f8f7673560ced1c32
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 16:05:51 +0200

    Update I18nProviderWrapper.jsx

--- components/I18nProviderWrapper.jsx
+++ components/I18nProviderWrapper.jsx
@@ -1,4 +1,6 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
 import '@/i18n'; // initialisiert i18next, erkannte Fehlermeldungen usw.
 
 export default function I18nProviderWrapper({ children }) {
