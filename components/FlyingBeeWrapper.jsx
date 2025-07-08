9f994167b7f286a896aece1b8eb26540e2f39649
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 09:43:23 +0200

    Update FlyingBeeWrapper.jsx

--- components/FlyingBeeWrapper.jsx
+++ components/FlyingBeeWrapper.jsx
@@ -1,1 +1,9 @@
+// components/FlyingBeeWrapper.jsx
+import dynamic from 'next/dynamic';
+
+const FlyingBee = dynamic(() => import('./FlyingBee'), {
+  ssr: false,
+});
+
+export default FlyingBee;
 
