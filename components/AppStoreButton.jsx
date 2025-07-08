b35d6d5c397eff49731c08898edbf2da61a29e06
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 16:04:11 +0200

    Update AppStoreButton.jsx

--- components/AppStoreButton.jsx
+++ components/AppStoreButton.jsx
@@ -1,4 +1,10 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
+import StickyHeader from '@/components/StickyHeader';
+import { useTranslation } from 'react-i18next';
+
+export default function RewardsPage() { /* ... */ }
 
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
