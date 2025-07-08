3e9fd706a2b468644cb0ed76337fc479d5bcab9e
Dibo27021990 <Dirkbock90@web.de>
2025-07-08 16:01:19 +0200

    Update page.jsx

--- app/adminlogin/page.jsx
+++ app/adminlogin/page.jsx
@@ -1,4 +1,10 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
+import StickyHeader from '@/components/StickyHeader';
+import { useTranslation } from 'react-i18next';
+
+export default function RewardsPage() { /* ... */ }
 
 import dynamic from 'next/dynamic';
 
