8bc71c73d9d17ff2b3bd80c7f604cb3f13a4fc1f
Dibo27021990 <Dirkbock90@web.de>
2025-07-10 16:31:06 +0200

    Update page.jsx

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,13 +1,8 @@
 'use client';
-export const dynamic = 'force-dynamic';
-
-import StickyHeader from '@/components/StickyHeader';
-import { useTranslation } from 'react-i18next';
-
-export default function RewardsPage() { /* ... */ }
-
 
 import dynamic from 'next/dynamic';
+import StickyHeader from '@/components/StickyHeader';
+import { useTranslation } from 'react-i18next';
 
 const AdminDashboardPage = dynamic(() => import('@/components/AdminDashboardPage'), {
   ssr: false,
