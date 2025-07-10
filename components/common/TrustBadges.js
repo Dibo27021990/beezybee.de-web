4fac2287dc6bab914e663438b73c68866887f586
Dibo27021990 <Dirkbock90@web.de>
2025-07-10 16:43:39 +0200

    Update TrustBadges.js

--- components/common/TrustBadges.js
+++ components/common/TrustBadges.js
@@ -1,38 +1,13 @@
 'use client';
-export const dynamic = 'force-dynamic';
 
-import StickyHeader from '@/components/StickyHeader';
-import { useTranslation } from 'react-i18next';
-
-export default function RewardsPage() { /* ... */ }
-
-
-
-import { useTranslation } from 'react-i18next';
 import Image from 'next/image';
 
-export default function Hero() {
-  const { t } = useTranslation();
-
+export default function TrustBadges() {
   return (
-    <section className="bg-white py-20 text-center relative">
-      <Image
-        src="/images/banner.jpg"
-        alt="Helperbee Banner"
-        width={120}
-        height={120}
-        className="mx-auto mb-4 rounded-full"
-      />
-      <h1 className="text-3xl sm:text-4xl font-bold mb-4">{t('hero.claim')}</h1>
-      <p className="text-lg text-gray-600 mb-6">{t('hero.slogan')}</p>
-      <div className="flex justify-center space-x-4">
-        <a href="#" className="bg-black text-white px-4 py-2 rounded">
-          {t('hero.downloadApp')}
-        </a>
-        <a href="#availability" className="bg-yellow-400 text-black px-4 py-2 rounded">
-          {t('hero.joinWaitlist')}
-        </a>
-      </div>
-    </section>
+    <div className="flex gap-4 justify-center items-center my-4">
+      <Image src="/images/trust1.svg" alt="Trust 1" width={80} height={80} />
+      <Image src="/images/trust2.svg" alt="Trust 2" width={80} height={80} />
+      <Image src="/images/trust3.svg" alt="Trust 3" width={80} height={80} />
+    </div>
   );
 }
